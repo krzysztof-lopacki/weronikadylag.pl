9cfa065dc3e5dac797363de459eace3af55f6799
Krzysztof Łopacki <krzysztof@lopacki.pl>
2023-02-12 19:45:55 +0100

    Create paitings.php

---
+++ paintings.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$title = "Corbet | Weronika Dyląg";
+include "blocks/head.php";
+?>
+
+<body>
+    <?php include "blocks/header.html"; ?>
+
+    <main>
+        <section>
+            <div class="container-fluid py-6">
+                <h1 class="in-top">Blog &mdash;<br>Stream of thoughts.</h1>
+            </div>
+            <div class="container-fluid">
+                <div class="grid">
+                    <div class="grid-sizer col-1"></div>
+                    <a class="grid-item col-4" href="purpose-of-art.html">
+                        <img alt="Weronika Dyląg" src="images/sicily_mosiac.jpg">
+                        <div class="caption p-3">
+                            <div class="title">The purpose of art</div>
+                        </div>
+                    </a>
+                    <a class="grid-item col-4" href="red.html">
+                        <img alt="Weronika Dyląg" src="images/red.jpg">
+                        <div class="caption p-3">
+                            <div class="title">Red</div>
+                        </div>
+                    </a>
+                    <a class="grid-item col-4" href="paint-folding.html">
+                        <img alt="Paint folding" src="images/off-canvas.png">
+                        <div class="caption p-3">
+                            <div class="title">Paint folding</div>
+                        </div>
+                    </a><a class="grid-item col-4" href="inspiration.html">
+                        <img alt="Weronika Dyląg" src="images/creature.jpg">
+                        <div class="caption p-3">
+                            <div class="title">Inspiration</div>
+                        </div>
+                    </a><a class="grid-item col-4" href="colour-palette.html">
+                        <img alt="Weronika Dyląg" src="images/colours.jpg">
+                        <div class="caption p-3">
+                            <div class="title">Colour palette</div>
+                        </div>
+                    </a><a class="grid-item col-4" href="vernissage.html">
+                        <img alt="Weronika Dyląg" src="images/vernissage.jpg">
+                        <div class="caption p-3">
+                            <div class="title">Colour palette</div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="container my-5 py-6">
+                <div class="row justify-content-md-center text-center">
+                    <div class="col-sm-5">
+                        <h4>Just random thoughts</h4>
+                        <p>Just some of my random thoughts about the art in general and the art I am making.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <?php include "blocks/footer.html"; ?>
+</body>
+
+</html>
