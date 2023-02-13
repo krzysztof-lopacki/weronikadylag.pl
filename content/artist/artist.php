ec6a3dc7aff976c85ab9aa778fb8bed64f37a0c1
Krzysztof Łopacki <krzysztof@lopacki.pl>
2023-02-13 23:16:48 +0100

    Add content to artist section

--- content/artist/artist.php
+++ content/artist/artist.php
@@ -6,14 +6,153 @@ $title = "Weronika Dyląg | Photography";
 include "/blocks/head.php";
 ?>
 
-<body>
-  <?php 
+<body class="artist">
+  <?php
   $headerClasses = "text-white";
-  include "/blocks/header.php"; 
+  include "/blocks/header.php";
   ?>
 
   <main>
-    
+    <section class="pt-0">
+      <div class="jumbotron jumbotron-fluid vh-50 d-flex align-items-center bg-dark text-white">
+        <div class="bg o-7" style="background-image:url(/content/artist/images/artist-header.jpg);"></div>
+        <div class="container text-center">
+          <h5 class="in-left">About artist</h5>
+          <h1 class="in-top semi-massive">Weronika Dyląg</h1>
+        </div>
+      </div>
+      <div class="container py-6">
+        <div class="row py-3">
+          <div class="col-auto">
+            <h1 class="semi-massive">Bio</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 py-5">
+            <p>
+              Weronika Aaron Dyląg is a Warsaw-based graphic designer who took up painting five years ago. Her graphic
+              design experience, very much visible in her early work, strongly influences her art. Her interest in
+              psychology and social interactions as well as her conviction that the ability to express and experience
+              positive emotions is a way of maintaining mental health dictates her colour palette.
+            </p>
+            <p>
+              In 2021, she became interested in acrylic paint, but working with fluid acrylic paint alone was not
+              satisfactory. Weronika began experimenting with acrylic mediums and paint proportions, eventually landing
+              on blowing the paint with air instead of using brushes.
+            </p>
+          </div>
+          <div class="col-sm-6 py-5 artist-photo-container" style="text-align:center;">
+            <img class="artist-photo" src="/content/artist/images/weronika-dylag.jpg" />
+          </div>
+        </div>
+
+        <div class="row py-3">
+          <div class="col-auto">
+            <h1 class="semi-massive"><br />Artist statement</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6 py-5">
+            <p>
+              Six years ago, I returned to painting working with acrylics. In my recent art, I focus on psychology and
+              positive emotions by dissecting colour, form and composition. My inspiration comes from the works of Yago
+              Hortal, Nicolaos Schizas, Orlanda Bloom, Lauren Mycroft, Jan Kalab as well as from my professional graphic
+              design experience.
+            </p>
+            <p>
+              To emphasize the elusiveness of emotional states and reality itself, I developed a technique that allows
+              me to work with fluid paint and flows of air with only a few brushstrokes. This technique allows me to
+              keep a thin layer of paint and create, with almost no brushstrokes, paintings in a print-like style. There
+              is a freeze of motion noticeable in the paintings and a paper-like paint folding created with a rapid
+              change of air direction.
+            </p>
+          </div>
+          <div class="col-sm-6 py-5">
+            <p>
+              I work on white, blue-ish, black, red and raw linen backgrounds – usually one colour to make the
+              interaction of the colours stand out.
+            </p>
+            <p>
+              My interest in psychology and the need to represent positive emotions in paintings were born out of a deep
+              conviction that mental health maintenance relies on a steady focus on positive aspects of life. Our
+              realities grow stale and difficult at times. I intend my work to be a stop where viewers can recharge and
+              energize to move forward against the adversities of life.
+            </p>
+          </div>
+        </div>
+
+        <div class="row py-3">
+          <div class="col-auto">
+            <h1 class="semi-massive"><br />Exhibitions</h1>
+          </div>
+        </div>
+        <div class="row py-4">
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Group exhibition "Ego"</h3>
+            <p>September 2021<br>Divulgarti Group<br>Palazzo Ducale, Genova, Italy</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Art3F Art Fair</h3>
+            <p>November 2021<br>represented by MONAT Gallery<br>Bruxelles, Belgium<br>(2 pieces exhibited, Sold piece
+              “Summer in bloom”)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">(un)fair art fair</h3>
+            <p>April 2022<br>represented by MONAT Gallery<br> Milan, Italy<br>(2 pieces exhibited)</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4"> Group exhibition “Visions from the unknown”</h3>
+            <p>April 2022 <br>MONAT Gallery<br>Madrid, Spain<br>(2 pieces exhibited)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Group exhibtion "Stripped"</h3>
+            <p>June 2022<br> AZUR Gallery<br> Madrid, Spain<br>(2 pieces exhibited)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4"> Art3F Art Fair</h3>
+            <p>June 2022<br>represented by MONAT Gallery<br>Monaco<br>(2 pieces exhibited)</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4"> Group exhibition “Euphoria”</h3>
+            <p>Septemer 2022 <br>AZUR Gallery<br>Madrid, Spain<br>(2 pieces exhibited)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Solo exhibtion "Colours of life"</h3>
+            <p>September 2022<br> 6th Seasons Distillery &amp; Patrycja Krześniak<br> Warsaw, Poland<br>(12 pieces
+              exhibited)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Opening exhibition</h3>
+            <p>October 2022<br>AZUR MIAMI Gallery, United States of America<br>Group exhibition - inauguration of the
+              gallery<br>(2 pieces exhibited)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Group exhibition "Unattainable"</h3>
+            <p>October 2022<br>MONAT Gallery<br>Group exhibition "UNATTAINABLE"<br>(2 pieces exhibited)</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Participation in a project</h3>
+            <p>COV-ART Project - Copelouzos Family Art Museum</p>
+          </div>
+          <div class="col-sm-4 py-3">
+            <h3 class="mb-4">Upcomming</h3>
+            <p>28th November 2022<br>to 13th January 2023<br>AZUR MIAMI Gallery, USA <br>Group exhibition<br>(2 pieces
+              exhibited)</p>
+            <p>16th December 2022<br>to 22nd 2023<br>MONAT Gallery, Spain<br>Group exhibition "The Restless Hour"<br>(2
+              pieces exhibited)</p>
+            <p>11th January 2023<br>to 8th February<br>SOLO Exhibition "Not obvious forms" - Museum of Krajeńska Land,
+              Poland</p>
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
   </main>
 
   <?php include "/blocks/footer.html"; ?>
