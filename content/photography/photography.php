514a9c6edcbe14afe4c249c64f70dccf454ef824
Krzysztof Łopacki <krzysztof@lopacki.pl>
2023-02-14 19:59:59 +0100

    Show photographs using php

--- content/photography/photography.php
+++ content/photography/photography.php
@@ -10,127 +10,57 @@ include "/blocks/head.php";
   <?php include "/blocks/header.php"; ?>
 
   <main>
+
     <section>
       <div class="container-fluid py-6">
-        <h1> <span class="d-block in-left">Landscape photography</span><span class="d-block in-top">Different parts of
-            the world</span></h1>
+        <h1 class="semi-massive"><span class="d-block in-left">Landscape photography</span><span class="d-block in-top">Different parts of the world</span></h1>
       </div>
+
       <div class="container-fluid">
-        <div class="grid">
+        <div class="grid gallery">
           <div class="grid-sizer col-1"></div>
-          <div class="grid-item col-6" href="#">
-            <img alt="Wadi rum" src="/content/photography/images/1b.jpg">
-            <div class="caption p-3">
-              <div class="title">Jordan</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Bugs" src="/content/photography/images/6a.jpg">
-            <div class="caption p-3">
-              <div class="title">Warsaw</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Wadi rum" src="/content/photography/images/3a.jpg">
-            <div class="caption p-3">
-              <div class="title">Jordan</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Greece" src="/content/photography/images/2a.jpg">
-            <div class="caption p-3">
-              <div class="title">Jordan</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Greece" src="/content/photography/images/4a.jpg">
-            <div class="caption p-3">
-              <div class="title">Greece</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/5a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/8a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/10a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/11a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/12a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/7a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/9a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/13a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/14a.jpg">
-            <div class="caption p-3">
-              <div class="title">Madeira</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/15a.jpg">
-            <div class="caption p-3">
-              <div class="title">Timna</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/16a.jpg">
-            <div class="caption p-3">
-              <div class="title">Timna</div>
-            </div>
-          </div>
-          <div class="grid-item col-3" href="#">
-            <img alt="Madera" src="/content/photography/images/17a.jpg">
-            <div class="caption p-3">
-              <div class="title">Timna</div>
-            </div>
-          </div>
+
+          <?php
+          // EDIT THIS LIST TO ADD/REMOVE/CHANGE ORDER OF THE PHOTOS
+          $photos = [
+            ["jordan-1", "Jordan"],
+            ["warsaw-1", "Warsaw"],
+            ["jordan-2", "Jordan"],
+            ["greece-1", "Greece"],
+            ["madeira-1", "Madeira"],
+            ["madeira-2", "Madeira"],
+            ["madeira-3", "Madeira"],
+            ["madeira-4", "Madeira"],
+            ["madeira-5", "Madeira"]
+          ];
+
+          foreach ($photos as $photo) {
+          ?>
+
+            <a class="lightbox grid-item col-3" href="/content/photography/images/<?= $photo[0] ?>.jpg"><img alt="<?= $photo[1] ?>" src="/content/photography/images/<?= $photo[0] ?>_thumbnail.jpg">
+              <div class="caption p-3">
+                <div class="title"><?= $photo[1] ?></div>
+              </div>
+            </a>
+          
+          <?php
+          }
+          ?>
+
         </div>
       </div>
+
       <div class="container my-5 py-6">
         <div class="row justify-content-md-center text-center">
-          <div class="col-sm-5">
+          <div class="col-sm-8">
             <h4>More works</h4>
             <p>For more works, please follow the instagram account or check
-            <div href="https://www.artsper.com/sg/contemporary-artists/poland/103743/weronica-dylag">Artspare</div> or <div href="https://www.artsy.net/artist/weronika-dylag">Artsy</div>.</p>
+              <a href="https://www.artsper.com/sg/contemporary-artists/poland/103743/weronica-dylag">Artspare</a> or <a href="https://www.artsy.net/artist/weronika-dylag">Artsy</a>.
+            </p>
           </div>
         </div>
       </div>
+
     </section>
   </main>
 
