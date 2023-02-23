9e9365f1563bd93566c7297ea192595540d91872
Krzysztof Łopacki <krzysztof@lopacki.pl>
2023-02-23 22:32:06 +0100

    Complete 'Not obvious forms' blog entry

---
+++ content/blog/not-obvious-forms/full.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$title = "Weronika Dyląg | Not obvious forms";
+include "/blocks/head.php";
+?>
+
+<body>
+    <?php include "/blocks/header.php"; ?>
+
+    <main>
+        <section>
+            <div class="container py-6">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-10">
+                        <h1 class="massive in-top">Not obvious forms</h1>
+                    </div>
+                </div>
+                <div class="row mt-5 justify-content-md-center">
+                    <div class="col-sm-10 in-left"><strong>Written by Weronika Dyląg</strong>
+                        <hr class="to-right">
+                        <div class="grey">18th January 2023</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container py-3">
+                <div class="row justify-content-md-center in-top">
+                    <div class="col-sm-6">
+                        <h2>The museum</h2>
+                        <p>On 11th of January the Krajenska Land Museum’s Director Mr. Tomasz Pasieka and me opened my first museum
+                            solo show. It was great to see that I was not the only one to be so excited about the exhibition – the
+                            museum’s director was too. It was a museum that helped me to take my first steps torwards art. It is a
+                            place where I started drawing, I discovered pastels and a place I had my first contact with oil paint.</p>
+                        <p>Musuem before “not obvious forms” was exhibiting objects associated with funeral rituals. Due to the
+                            exhibition the whole floor was painted in dark gray creating this gloomy, weighing you down atmosphere. It
+                            was a well-organized with exhibits landed by three different museums but just like funerals in our culture
+                            (and many others in western countries), left me with <em>memento mori-kind</em> of feeling.</p>
+                        <p>With those surroundings it was hard to imagine the impact my bright and colourful art can have on the
+                            environment where it will be displayed. Left with this uneasy feeling, worried that my art will not be
+                            able to brighten up these gray walls, I was just hoping that after all exhibits will be return to the
+                            original museums the place will change enough to create at least neutral space.</p>
+                        <p></p><img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/not-obvious-forms/images/vernissage_1.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container py-3">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-6">
+                        <h3>Surprise</h3>
+                        <p>Boy, I was surprised… For me the transformation of the museum was like those nasty and horribly looking
+                            Gremlins that ate after midnight or were in contact with water went back being those cute big black eyes
+                            balls of fur. Just after seeing first images sent by Mr. Pasieka right before hanging I was stunt how the
+                            place changed. Seeing the artworks on the walls was even more exciting as the place started to be filled
+                            with life.</p>
+                        <p>I was not the only one who was a bit speechless at first. An elder man who approached me just after I
+                            finished talking, who said that due to his profession he saw in his life many art shows, but when he went
+                            up the stairs and reached the floor with the exhibition he was left without words and then he just
+                            congratulated me on the show. For me it is the sweetest thing anyone could say about my art.</p>
+                        <p>And this is yet another reason why artists should exhibit not only in big museums, but in those close to
+                            the people, those created by the communities and for the communities outside big cities. To make art more
+                            accessible.</p>
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/not-obvious-forms/images/vernissage_2.jpg">
+                    </div>
+
+
+
+
+
+
+
+
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include "/blocks/footer.html"; ?>
+</body>
+
+</html>
