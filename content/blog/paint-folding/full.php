ffc9f0bf4e713b0d3a96b593453ffa68a146596e
Krzysztof Łopacki <krzysztof@lopacki.pl>
2023-02-23 22:35:42 +0100

    Complete  blog entry

---
+++ content/blog/paint-folding/full.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$title = "Weronika Dyląg | Paint folding";
+include "/blocks/head.php";
+?>
+
+<body>
+    <?php include "/blocks/header.php"; ?>
+
+    <main>
+        <section>
+            <div class="container py-6">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-10">
+                        <h1 class="massive in-top">Paint folding</h1>
+                    </div>
+                </div>
+                <div class="row mt-5 justify-content-md-center">
+                    <div class="col-sm-10 in-left"><strong>Written by Weronika Dyląg</strong>
+                        <hr class="to-right">
+                        <div class="grey">24th November 2022</div>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-3">
+                <div class="row justify-content-md-center in-top">
+                    <div class="col-sm-6">
+                        <h2>Working with a fluid paint is a challenge in it-self.</h2>
+                        <p><a href="https://en.wikipedia.org/wiki/Helen_Frankenthaler">Hellen Frankenthaler’s</a> was this amazing
+                            women who started working with fluid oil paint. Her works intrigued with a watercolour-like touch.
+                            <a href="https://www.npr.org/2021/03/23/977919763/with-fierce-poise-helen-frankenthaler-poured-beauty-onto-canvas">“Off
+                                White Square”</a> I think is my favourite work of hers. It’s vivid and delicate at the same time. But
+                            many of her works were delicate precisely because she was using fluid paint. She had to dilute the paint
+                            in order to be able to work with fluid paint. Today we can work with different mediums that can help to
+                            liquefy the paint without thinning it and loosing pigment.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="container py-3">
+                <div class="row justify-content-md-center in-top">
+                    <div class="col-sm-6">
+                        <h3>But adding air to it… well expect unexpected.</h3>
+                        <p>The challenge remains. When using different mediums the paint is appearing as a different shade. You need
+                            to remember what shade it will become after the paint will dry. When the liquids mix together and will
+                            give you a new colour, it’s shade will also be different when dry. But when working with air the biggest
+                            challenge is how to control the fluid and the blows of air. And planning ahead is important.</p>
+                        <p>If you’d be watching me from a far just before I start a painting you would see somebody who is walking
+                            around the canvas and like conductor or a dancer is swaying hands in different directions with changing
+                            rhythm - so to polish standard - a lunatic. But this is a moment I am doing a mental exercise. I am
+                            imagining not only the paint I will pour on the canvas but also the directions it will be blown and the
+                            movement I need to do in order to fold the paint.</p>
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/paint-folding/images/off-canvas.png">
+
+                    </div>
+                </div>
+            </div>
+            <div class="container py-3">
+                <div class="row justify-content-md-center in-top">
+                    <div class="col-sm-6">
+                        <h3>Folding paint</h3>
+                        <p>So how do you fold a paint? And can you even fold something that is a liquid and is laying on a flat
+                            surface?</p>
+                        <p>At many of my paintings I am using the force to push the paint across the canvases and to push it out.
+                            And on all of my paintings you are able to see this movement. But while working on “Look inside” painting,
+                            I discovered that I can also fold it. What do I mean? Well the paint is rapidly changing direction
+                            creating a new layer of paint without destroying the layer beneath, creating this sort of paper folding
+                            effect.</p>
+                        <p>This also a way to have control over the paint and being able to create central composition that is
+                            entirely closed within the canvas. </p>
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/paint-folding/images/self-awareness.jpg">
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <?php include "/blocks/footer.html"; ?>
+</body>
+
+</html>
