c6f671bd06d35f7e892302d476e256f18498c620
Krzysztof Łopacki <krzysztof@lopacki.pl>
2023-02-23 22:47:21 +0100

    Complete 'Purpose of art' blog entry

---
+++ content/blog/purpose-of-art/full.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+$title = "Weronika Dyląg | Purpose of art";
+include "/blocks/head.php";
+?>
+
+<body>
+    <?php include "/blocks/header.php"; ?>
+
+    <main>
+        <section>
+            <div class="container py-6">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-10">
+                        <h1 class="massive in-top">The purpose of art</h1>
+                    </div>
+                </div>
+                <div class="row mt-5 justify-content-md-center">
+                    <div class="col-sm-10 in-left"><strong>Written by Weronika Dyląg</strong>
+                        <hr class="to-right">
+                        <div class="grey">8th November 2022</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container py-6">
+                <div class="row justify-content-md-center in-top">
+                    <div class="col-sm-6">
+                        <h2>Sicily</h2>
+                        <p>We recently took a short trip to Sicily. Among eating italian food, walking around the cities and getting
+                            way up to see Etna up close, we were sightseeing.</p>
+                        <p>Our first stop was <a href="https://en.wikipedia.org/wiki/Villa_Romana_del_Casale">the Villa Romana del
+                                Casale</a>. Beautiful place and a great opportunity to see mosaic floors. Rooms designeted for work and
+                            for servants had decorative patterns. Those designeted for the family were created to tell stories.</p>
+                    </div>
+                </div>
+            </div>
+
+            <img class="img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/purpose-of-art/images/sicily_mosaic_0.jpg">
+
+            <div class="container py-6">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-6">
+                        <h3>Diferent puposes</h3>
+                        <p>The purpose of those beautiful floor mosaics was to tell a story of a great hunt or to show the exotic
+                            animals the family was importing from Africa to Rome. Another goal was to show status and to intimidate
+                            (some mosaics were showing what happens to unwilling to cooperate slaves).</p>
+                        <p>The mosaics were serving more than one purpose. They were more than just being a decorative floor.</p>
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/purpose-of-art/images/sicily_mosaic_2.jpg">
+
+                        <p>After that, we went to <a href="https://en.wikipedia.org/wiki/Agrigento">Agrigento</a> - known for the
+                            ruins of the ancient city of Akragas in the Valley of the Temples from hellenistic period. You can find
+                            there a bit more than expected. And that is the place you can start thinking about yet another purpose of
+                            art - relgious.</p>
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/purpose-of-art/images/sicily_2.jpg">
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/purpose-of-art/images/sicily_1.jpg">
+
+                        <p>And in the evening we started driving to Monreale. As we parked car we decided that we're hungry and
+                            started looking for food. That is how we discovered a lovely place that was serving pizza and was
+                            displaying local artists - <a href="https://www.google.com/search?q=monreale+pizzeria+tot%C3%B2&rlz=1C1CHBD_plPL1003PL1003&oq=monreale+pizzeria+&aqs=chrome.2.69i57j0i19i512l3j0i19i22i30l6.19482j0j1&sourceid=chrome&ie=UTF-8">Pizzeria
+                                Art Libri Totó</a>. Next day, we went to see the purpose of our arrival in the city of Monreale - <a href="https://en.wikipedia.org/wiki/Monreale_Cathedral">Monreale Cathedral</a> - 12th century cathedral
+                            with golden mosaics all over the interrior. And this is a place where you cannot escape the religious
+                            purpose that art was serving for centuries.<br><br>The cathedral is a masterpiece. And just like the
+                            mosaics from the roman villa the mosaics here are also telling stories. Biblical stories on the walls of
+                            churches had educational purposes as not all taking part in masses were literate.</p>
+                        <img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/purpose-of-art/images/sicily_mosaic_3.jpg">
+
+                        <p>But there was a decorative purpse to it as well. Many of the mosaics were patterns - on the walls and
+                            floors and they were there solely for the decorative purpose and no other. But the cathedral was build to
+                            humiliate the bishop of Palermo - it was build to show of the power and status and on unimaginable speed.
+                            
+                        </p><img class="mt-2 mb-4 img-fluid vw-100" alt="Blog by Weronika Dyląg" src="/content/blog/purpose-of-art/images/sicily_mosaic_4.jpg">
+                    </div>
+                </div>
+            </div>
+
+            <div class="container py-6">
+                <div class="row justify-content-md-center">
+                    <div class="col-sm-6">
+                        <h3>Now</h3>
+                        <p>In the past patrons of the arts were indirectly or directly influencing the purpos of the art -
+                            decorative, educational, religious, to show power and status or to simply show how somebody looks like.
+                            Middle ages portraits were todays selfies with instagram filters on. The art was suppost be mimicing the
+                            real life but in a grandier way (women were more beautiful, the battles were bigger, and men were
+                            younger).<br><br>But than the philosophy changed. The social dynamics changed. And so did art. Hilma af
+                            Klint (before anyone we know of) started exploring what other purposes art can serve (death of her sister
+                            deffinitely had something to do with that as well). She started discovering that we do not have to turn
+                            ourselves to photorealism - photography took over. She started exploring automatic drawing, geometric
+                            forms, and moved to more fluid forms. She discovered astractionsm before Kandinsky or Mondrain.<br><br>But
+                            what about now? What are the functions of visual arts today? We do not need to mimic the world anymore, we
+                            can but we do not have to. The&nbsp;patrons of the arts are different now. The&nbsp;whole enviroment is
+                            different than it was a century ago. So what functions beside decorative the visual arts can take over?
+                        </p>
+                        <p>From what I observe a lot of art is done to create change - political change, social change, change of
+                            perspective. We want to change the world and our tool to express what aspect of the world we are
+                            interested in changing is art. Some are interested in climate change as the subject of their work, some
+                            others are interested in the subject of minorities (none-heteronormative, races, women? - can we be
+                            encountered for a minority in terms of our rights etc?).<br><br> I am interested in our psychology. I
+                            belive that the only way to force change (social/political) is by changing ourselves. It's like in the
+                            song:<br><br> &quot;If you wanna make the world a better place<br>Take a look at yourself and then make a
+                            change&quot;<br><br>And I firmly belive that people that are truly in content, in agreement with
+                            themselves (and they are not psychopaths), that are not doing things from the position of insecurities,
+                            fragile ego, yearning for love they never had etc. are the people who make this world a better place. The
+                            way for us to move forward, I believe is being true to ourselves and with ourselves. Is to fill the empty
+                            spaces in our egos or to deflate our egos. But not in a demeaning way. We need to feel good with
+                            ourselves, in our bodies, in our comunities to be able to press any other change. And we cannot do that if
+                            the only messages we get are depressing, demeaning, agressive, mocking.<br><br>So the purpose of my work
+                            is to empesise the importance of our mental health, to focus our attention to our needs, to positive
+                            emotions and to educate about mental health through art. What purpose are you serving?<br><br></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+    <?php include "/blocks/footer.html"; ?>
+</body>
+
+</html>
